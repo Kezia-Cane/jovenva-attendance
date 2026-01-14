154b0274f122fcc2e892ff8f1ac6f1279390466c
Kesh <59233228+Kezia-Cane@users.noreply.github.com>
2026-01-14 14:29:18 +0800

    Purity UI Integration

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: "var(--background)",
+                foreground: "var(--foreground)",
+                "teal-300": "var(--purity-teal)",
+                "teal-400": "var(--purity-teal-hover)",
+                "gray-700": "#2D3748",
+                "purity-700": "var(--purity-gray-dark)",
+            },
+            fontFamily: {
+                sans: ["Roboto", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
+export default config;
